003abae1c5d9214f100e8b323e3a48072e043727
Mukulkanojia123 <mkanojia1996@gmail.com>
2025-03-11 23:17:52 +0530

    admin Layour

--- client/src/components/layout/AdminLayout.jsx
+++ client/src/components/layout/AdminLayout.jsx
@@ -1,12 +1,97 @@
 import Grid from '@mui/material/Grid2';
 import React, { useState } from 'react'
-import { grayColor } from '../constants/color';
-import { Box, Drawer, IconButton, Stack, } from '@mui/material';
-import { Close as CloseIcon, Menu as MenuIcon } from '@mui/icons-material';
+import { grayColor, matBlack } from '../constants/color';
+import { Box, Drawer, IconButton, Stack, styled, Typography, } from '@mui/material';
+import { Close as CloseIcon, Dashboard as DashboardIcon, ExitToApp as ExitToAppIcon, Groups as GroupIcon, ManageAccounts as ManageAccountsIcon, Menu as MenuIcon, Message as MessageIcon} from '@mui/icons-material';
+import { useLocation, Link as LinkComponent} from 'react-router-dom';
+// import { adminTabs } from '../constants/route';
+// import { Link } from '../styles/StyledComponents';
 
+
+const Link = styled(LinkComponent)`
+ text-decoration : none;
+ border-radius : 2rem;
+ padding : 1rem 2rem;
+ color : black;
+ & : hover {
+ color : rgba(0,0,0,0.54);
+ }
+
+`
+
+
+const adminTabs = [
+    {
+    name : "Dashboard",
+    path : "/admin/dashboard",
+    icon : <DashboardIcon/>
+},
+    {
+    name : "Users",
+    path : "/admin/users-management",
+    icon : <ManageAccountsIcon/>
+},
+    {
+    name : "Chat",
+    path : "/admin/chats-management",
+    icon : <GroupIcon/>
+},
+    {
+    name : "messages",
+    path : "/admin/messages",
+    icon : <MessageIcon/>
+},
+]
 const Sidebar = (w = "100%")=>{
-    return <Stack width={w}></Stack>
-} 
+
+    const location = useLocation();
+
+    const logoutHandler = () =>{
+        console.log('logout')
+    }
+
+    return (
+    <Stack 
+    width={w} 
+    direction={'column'} 
+    p={'3rem'} 
+    spacing={'3rem'}
+    >
+        <Typography variant='h5' textTransform={'uppercase'}>
+            ChatApp
+        </Typography>
+    <Stack spacing={'1rem'}>
+    {
+        adminTabs.map((tab, index) =>(
+            <Link key={tab.path} to={tab.path}
+            sx={
+                location.pathname === tab.path && {
+                    bgcolor : matBlack,
+                    color : 'white',
+                    " : hover" : {color : 'white'}
+                }
+            }
+            >
+                <Stack direction={'row'} alignItems={'center'} spacing={'1rem'}>
+                {
+                    tab.icon
+                }
+                <Typography>{tab.name}</Typography>
+                </Stack>
+            </Link>
+        ))
+    }
+    <Link
+     onClick={logoutHandler}
+     >
+        <Stack direction={'row'} alignItems={'center'} spacing={'1rem'}>
+                <ExitToAppIcon/>
+            <Typography>{tab.name}</Typography>
+        </Stack>
+    </Link>
+    </Stack>
+    </Stack>
+)} 
 
 const AdminLayout = ({children}) => {   // that is component that wrrap under admin layout
     
