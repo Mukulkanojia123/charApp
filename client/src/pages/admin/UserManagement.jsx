d6df5d72590b43a30dab1c6287c2d0e077428823
Mukulkanojia123 <mkanojia1996@gmail.com>
2025-03-16 13:37:12 +0530

    Usermanagement and table

--- client/src/pages/admin/UserManagement.jsx
+++ client/src/pages/admin/UserManagement.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
 import AdminLayout from '../../components/layout/AdminLayout'
+import Table from '../../components/shared/Table'
+import { Avatar } from '@mui/material'
 
+
+const columns = [
+  {
+    field : 'id',
+    headerName : 'ID',
+    headerClassName : 'table-header',
+    width : 200
+  },
+  {
+    field : 'avatar',
+    headerName : 'Avatar',
+    headerClassName : 'table-header',
+    width : 150,
+    renderCell: (params) => (
+    <Avatar 
+    alt={params.row.name} 
+    src={params.row.avatar}/>
+    )
+  },
+  {
+    field : "name",
+    headerName : "Name",
+    headerClassName : 'table-header',
+    width : 200,
+  },
+  {
+    field : "username",
+    headerName : "Username",
+    headerClassName : 'table-header',
+    width : 200,
+  },
+  {
+    field : "friends",
+    headerName : "friends",
+    headerClassName : 'table-header',
+    width : 150,
+  },
+  {
+    field : "groups",
+    headerName : "groups",
+    headerClassName : 'table-header',
+    width : 200,
+  },
+]
 const UserManagement = () => {
+
+  const [rows, setRows] = useState([])
+
   return (
     <AdminLayout>
-        <div>Usermanagement</div>
+        <Table 
+        heading={'All Users'} 
+        columns={columns} 
+        rows={rows} 
+        />
     </AdminLayout>
   )
 }
