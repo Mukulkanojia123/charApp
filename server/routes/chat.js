54a414980de051a3d474dbb61e2d2dae95ec8397
Mukulkanojia123 <mkanojia1996@gmail.com>
2025-04-14 22:34:24 +0530

    some chat route change

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { addMembers, deleteChat, getChatDetails, getMyChat, getMyGroup, leaveGroup, newGroupChat, removeMember, renameGroup, sendAttachments } from "../controllers/chat.js";
+import { addMembers, deleteChat, getChatDetails, getMyChat, getMyGroup, leaveGroup, newGroupChat, removeMember, renameGroup, sendAttachments, getMessages } from "../controllers/chat.js";
 import { isAuthenticate } from "../middlewares/auth.js";
 import { attachmentsMulter } from "../middlewares/multer.js";
+import { addMemberValidator, chatIdValidator, newGroupValidator, removeMemberValidator, sendAttachmentsValidator, validateHandler } from "../lib/validators.js";
 
 
 const app = express.Router();
@@ -10,16 +11,19 @@ const app = express.Router();
 
 app.use(isAuthenticate)
 
-app.post('/new', newGroupChat)
+app.post('/new', newGroupValidator() ,validateHandler,newGroupChat)
 app.post('/my', getMyChat)
 app.post('/my/groups', getMyGroup)
-app.put("/addmember", addMembers)
-app.put("/removemember", removeMember)
+app.put("/addmember", addMemberValidator(),validateHandler, addMembers)
+app.put("/removemember", removeMemberValidator(), validateHandler, removeMember)
 
-app.delete('/leave/:id', leaveGroup)
+app.delete('/leave/:id', chatIdValidator(),validateHandler,leaveGroup)
 
 
-app.post('/message', attachmentsMulter, sendAttachments)
+app.post('/message', attachmentsMulter, sendAttachmentsValidator() ,validateHandler,sendAttachments)
+
+// Get Messages
+app.get("/message/:id", chatIdValidator(), validateHandler, getMessages);
 
 app.route('/:id')
 .get(getChatDetails)
