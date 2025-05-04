bc885e7d24a2fcf365672715e2eba55f7c9809a4
Mukulkanojia123 <mkanojia1996@gmail.com>
2025-05-04 12:51:39 +0530

    some fix body

---
+++ client/src/Body.jsx
@@ -0,0 +1,82 @@
+import React, { lazy } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import ProtectRoute from './components/auth/ProtectRoute.jsx';
+
+
+
+const Home = lazy(() => import('./pages/Home.jsx'));
+const Login = lazy(() => import('./pages/Login'));
+const Chat = lazy(() => import('./pages/Chat'));
+const Groups = lazy(() => import('./pages/Groups'));
+const NotFound = lazy(() => import('./pages/NotFound.jsx'));
+const AdminLogin = lazy(() => import('./pages/admin/AdminLogin.jsx'))
+const Dashboard = lazy(() => import('./pages/admin/Dashboard.jsx'))
+const UserManagement = lazy(() => import('./pages/admin/UserManagement.jsx'))
+const ChatManagement = lazy(() => import('./pages/admin/ChatManagement.jsx'))
+const MessagesManagement = lazy(() => import('./pages/admin/MessageManagement.jsx'))
+
+
+
+
+const Body = () => {
+
+    const user = true;
+
+// Router configuration
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <ProtectRoute user={user} />,
+        children: [
+          { path: '/', element: <Home /> },
+          { path: '/chat/:chatId', element: <Chat /> },
+          { path: '/group', element: <Groups /> },
+        ],
+      },
+      {
+        path: '/login',
+        element: (
+          <ProtectRoute user={!user} redirect="/">
+            <Login />
+          </ProtectRoute>
+        ),
+      },
+      {
+        path : '/admin',
+        element : <AdminLogin/>
+      },
+      {
+        path : '/admin/dashboard',
+        element : <Dashboard/>
+      },
+      {
+        path : '/admin/users-management',
+        element : <UserManagement/>
+      },
+      {
+        path : '/admin/chats-management',
+        element : <ChatManagement/>
+      },
+      {
+        path : '/admin/messages-management',
+        element : <MessagesManagement/>
+      },
+      { path: '*', element: <NotFound /> },
+    ],
+  },
+]);
+
+
+  return (
+    <>
+        <RouterProvider router={appRouter} />
+    </>
+  )
+}
+
+export default Body
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,77 +1,14 @@
-import React, { StrictMode, lazy, Suspense } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
 import { CssBaseline } from '@mui/material';
+import React, { StrictMode, Suspense } from 'react';
+import { createRoot } from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ProtectRoute from './components/auth/ProtectRoute.jsx';
+import { Provider } from 'react-redux';
 import { LayoutLoader } from './components/layout/Loaders.jsx';
-import {Provider} from 'react-redux'
-import store from './redux/store.js'
+import store from './redux/store.js';
+
+import Body from './body.jsx';
 
 
-// Lazy-loaded components
-const Home = lazy(() => import('./pages/Home.jsx'));
-const Login = lazy(() => import('./pages/Login'));
-const Chat = lazy(() => import('./pages/Chat'));
-const Groups = lazy(() => import('./pages/Groups'));
-const NotFound = lazy(() => import('./pages/NotFound.jsx'));
-const AdminLogin = lazy(() => import('./pages/admin/AdminLogin.jsx'))
-const Dashboard = lazy(() => import('./pages/admin/Dashboard.jsx'))
-const UserManagement = lazy(() => import('./pages/admin/UserManagement.jsx'))
-const ChatManagement = lazy(() => import('./pages/admin/ChatManagement.jsx'))
-const MessagesManagement = lazy(() => import('./pages/admin/MessageManagement.jsx'))
-
-
-const user = true;
-
-// Router configuration
-const appRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <ProtectRoute user={user} />,
-        children: [
-          { path: '/', element: <Home /> },
-          { path: '/chat/:chatId', element: <Chat /> },
-          { path: '/group', element: <Groups /> },
-        ],
-      },
-      {
-        path: '/login',
-        element: (
-          <ProtectRoute user={!user} redirect="/">
-            <Login />
-          </ProtectRoute>
-        ),
-      },
-      {
-        path : '/admin',
-        element : <AdminLogin/>
-      },
-      {
-        path : '/admin/dashboard',
-        element : <Dashboard/>
-      },
-      {
-        path : '/admin/users-management',
-        element : <UserManagement/>
-      },
-      {
-        path : '/admin/chats-management',
-        element : <ChatManagement/>
-      },
-      {
-        path : '/admin/messages-management',
-        element : <MessagesManagement/>
-      },
-      { path: '*', element: <NotFound /> },
-    ],
-  },
-]);
 
 // Root rendering with global Suspense
 createRoot(document.getElementById('root')).render(
@@ -81,7 +18,7 @@ createRoot(document.getElementById('root')).render(
       <CssBaseline />
       <div onContextMenu={(e) => e.preventDefault()}>
       <Suspense fallback={<LayoutLoader/>}>
-        <RouterProvider router={appRouter} />
+        <Body/>
       </Suspense>
       </div>
     </HelmetProvider>
