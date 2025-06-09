6ef4f03706e9349c854789ce203b69d1ae330856
Mukulkanojia123 <mkanojia1996@gmail.com>
2025-06-09 12:28:05 +0530

    adminLogin

--- client/src/components/layout/AdminLayout.jsx
+++ client/src/components/layout/AdminLayout.jsx
@@ -2,8 +2,10 @@ import Grid from '@mui/material/Grid2';
 import React, { useState } from 'react'
 import { grayColor, matBlack } from '../constants/color';
 import { Box, Drawer, IconButton, Stack, styled, Typography, } from '@mui/material';
-import { Close as CloseIcon, Dashboard as DashboardIcon, ExitToApp as ExitToAppIcon, Groups as GroupIcon, ManageAccounts as ManageAccountsIcon, Menu as MenuIcon, Message as MessageIcon} from '@mui/icons-material';
-import { useLocation, Link as LinkComponent, Navigate} from 'react-router-dom';
+import { Close as CloseIcon, Dashboard as DashboardIcon, ExitToApp as ExitToAppIcon, Groups as GroupIcon, ManageAccounts as ManageAccountsIcon, Menu as MenuIcon, Message as MessageIcon } from '@mui/icons-material';
+import { useLocation, Link as LinkComponent, Navigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { adminLogout } from '../../redux/thunks/admin';
 // import { adminTabs } from '../constants/route';
 // import { Link } from '../styles/StyledComponents';
 
@@ -22,131 +24,136 @@ const Link = styled(LinkComponent)`
 
 const adminTabs = [
     {
-    name : "Dashboard",
-    path : "/admin/dashboard",
-    icon : <DashboardIcon/>
-},
+        name: "Dashboard",
+        path: "/admin/dashboard",
+        icon: <DashboardIcon />
+    },
     {
-    name : "Users",
-    path : "/admin/users-management",
-    icon : <ManageAccountsIcon/>
-},
+        name: "Users",
+        path: "/admin/users-management",
+        icon: <ManageAccountsIcon />
+    },
     {
-    name : "Chat",
-    path : "/admin/chats-management",
-    icon : <GroupIcon/>
-},
+        name: "Chat",
+        path: "/admin/chats-management",
+        icon: <GroupIcon />
+    },
     {
-    name : "messages",
-    path : "/admin/messages",
-    icon : <MessageIcon/>
-},
+        name: "messages",
+        path: "/admin/messages",
+        icon: <MessageIcon />
+    },
 ]
-const Sidebar = (w = "100%")=>{
+const Sidebar = (w = "100%") => {
 
     const location = useLocation();
+    const dispatch = useDispatch();
 
-    const logoutHandler = () =>{
-        console.log('logout')
-    }
+    const logoutHandler = () => {
+        dispatch(adminLogout());
+    };
 
     return (
-    <Stack 
-    width={w} 
-    direction={'column'} 
-    p={'3rem'} 
-    spacing={'3rem'}
-    >
-        <Typography variant='h5' textTransform={'uppercase'}>
-            ChatApp
-        </Typography>
-    <Stack spacing={'1rem'}>
-    {
-        adminTabs.map((tab, index) =>(
-            <Link key={tab.path} to={tab.path}
-            sx={
-                location.pathname === tab.path && {
-                    bgcolor : matBlack,
-                    color : 'white',
-                    " : hover" : {color : 'white'}
-                }
-            }
-            >
-                <Stack direction={'row'} alignItems={'center'} spacing={'1rem'}>
+        <Stack
+            width={w}
+            direction={'column'}
+            p={'3rem'}
+            spacing={'3rem'}
+        >
+            <Typography variant='h5' textTransform={'uppercase'}>
+                ChatApp
+            </Typography>
+            <Stack spacing={'1rem'}>
                 {
-                    tab.icon
+                    adminTabs.map((tab, index) => (
+                        <Link key={tab.path} to={tab.path}
+                            sx={
+                                location.pathname === tab.path && {
+                                    bgcolor: matBlack,
+                                    color: 'white',
+                                    " : hover": { color: 'white' }
+                                }
+                            }
+                        >
+                            <Stack direction={'row'} alignItems={'center'} spacing={'1rem'}>
+                                {
+                                    tab.icon
+                                }
+                                <Typography>{tab.name}</Typography>
+                            </Stack>
+                        </Link>
+                    ))
                 }
-                <Typography>{tab.name}</Typography>
-                </Stack>
-            </Link>
-        ))
-    }
-    <Link
-     onClick={logoutHandler}
-    >
-        <Stack direction={'row'} alignItems={'center'} spacing={'1rem'}>
-            <ExitToAppIcon/>
-            <Typography fontSize={'1.2rem'}>logout</Typography>
+                <Link
+                    onClick={logoutHandler}
+                >
+                    <Stack direction={'row'} alignItems={'center'} spacing={'1rem'}>
+                        <ExitToAppIcon />
+                        <Typography fontSize={'1.2rem'}>logout</Typography>
+                    </Stack>
+                </Link>
+            </Stack>
         </Stack>
-    </Link>
-    </Stack>
-    </Stack>
-)} 
+    )
+}
 
 
 
 const isAdmin = true;
 
-const AdminLayout = ({children}) => {   // that is component that wrrap under admin layout
-    
+const AdminLayout = ({ children }) => {   // that is component that wrrap under admin layout
+
+    const { isAdmin } = useSelector((state) => state.auth);
+
     const [isMobile, setIsMobile] = useState(false);
 
-    const handleMobile = ()=> setIsMobile(!isMobile);
+    const handleMobile = () => setIsMobile(!isMobile);
+
     const handleClose = () => setIsMobile(false);
 
-    if(!isAdmin) return <Navigate to='/admin'/>
-   
+    if (!isAdmin) return <Navigate to="/admin" />;
 
-  return (
-    <Grid container minHeight={'100vh'}>
-        <Box
-        sx={{
-            display : {xs : 'block', md : "none"},
-            position : 'fixed',
-            right : '1rem',
-            top : '1rem'
-        }}>
-            <IconButton onClick={handleMobile}>
-            {
-                isMobile ? <CloseIcon/> :  <MenuIcon/>
-            }
-           
-            </IconButton>
-            
-        </Box>
-        <Grid
-        item
-        size={{ lg: 3, md: 4 }}
-        sx={{
-         display : {xs : 'none', md:'block'}   
-        }}
-        >
-        <Sidebar/>
+
+    return (
+        <Grid container minHeight={'100vh'}>
+            <Box
+                sx={{
+                    display: { xs: 'block', md: "none" },
+                    position: 'fixed',
+                    right: '1rem',
+                    top: '1rem'
+                }}>
+                <IconButton onClick={handleMobile}>
+                    {
+                        isMobile ? <CloseIcon /> : <MenuIcon />
+                    }
+
+                </IconButton>
+
+            </Box>
+            <Grid
+                item
+                size={{ lg: 3, md: 4 }}
+                sx={{
+                    display: { xs: 'none', md: 'block' }
+                }}
+            >
+                <Sidebar />
+            </Grid>
+            <Grid
+                item
+                size={{ xs: 12, lg: 9, md: 8 }}
+                sx={{
+                    bgcolor: grayColor
+                }}
+            >
+                {children}
+            </Grid>
+            <Drawer open={isMobile} onClose={handleClose}>
+                <Sidebar w="50vw" />
+            </Drawer>
         </Grid>
-        <Grid
-        item
-        size={{ xs :12 ,lg: 9, md: 8 }}
-        sx={{
-            bgcolor : grayColor   
-        }}
-        >
-            {children}
-        </Grid>
-        <Drawer open={isMobile} onClose={handleClose}>
-        <Sidebar w="50vw"/>
-        </Drawer>
-    </Grid>
-  )
+    )
 }
 
 export default AdminLayout
--- client/src/pages/admin/AdminLogin.jsx
+++ client/src/pages/admin/AdminLogin.jsx
@@ -1,64 +1,91 @@
-import React, { useState } from 'react'
-import { Stack,Button, Container, Paper, Typography, Avatar, IconButton } from '@mui/material'
+import { useInputValidation } from '6pp';
+import { Button, Container, Paper, Typography, TextField } from '@mui/material';
 import TextField from '@mui/material/TextField';
-import {CameraAlt as CameraAltIcon} from '@mui/icons-material'
-import { VisuallyHiddenInput } from './../../components/styles/StyledComponents';
-import {useFileHandler, useInputValidation, useStrongPassword} from '6pp'
-import { usernamevalidator } from './../../utils/validators';
+import { useDispatch, useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import { adminLogin, getAdmin } from '../../redux/thunks/admin';
 
-const isAdmin = false
+// const isAdmin = false
 const AdminLogin = () => {
 
-    const secretKey = useInputValidation("")
+    const { isAdmin } = useSelector((state) => state.auth);
 
-    const submitHandler = (e)=>{
-        e.preventDefault();
-    }
+  const dispatch = useDispatch();
 
-    if(isAdmin) return <Navigate to="/admin/dashboard"/>
+    const secretKey = useInputValidation("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(adminLogin(secretKey.value));
+  };
+
+  useEffect(() => {
+    dispatch(getAdmin());
+  }, [dispatch]);
+
+  if (isAdmin) return <Navigate to="/admin/dashboard" />;
+
 
   return (
     <div
-        style={{
-            backgroundImage : "linear-gradient(rgb(255, 225, 209), rgb(249, 159, 159))"
-        }}
-        >
-        <Container 
-        component={"main"} 
-        maxWidth='xs' 
+      style={{
+        backgroundImage: bgGradient,
+      }}
+    >
+      <Container
+        component={"main"}
+        maxWidth="xs"
         sx={{
-            height:"100vh",
-            display: 'flex',
-            justifyContent : 'center',
-            alignItems : 'center'
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
         }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            padding: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-            <Paper elevation={3} sx={{ padding: 4,
-                 display: 'flex', 
-                 flexDirection: 'column', 
-                 alignItems: 'center',
-                //  width: '100%' 
-                 }}>
-                
-                    <Typography variant='h5'>Admin Login</Typography>
-                    <form
-                    style={{
-                        width : '100%',
-                        marginTop : '1rem'
-                    }}
-                    onSubmit={submitHandler}
-                    >
-                        
-                        <TextField required fullWidth label="SecretKey" type="SecretKey"margin="normal" variant="outlined" value={secretKey.value} onChange={secretKey.changeHandler}/>
-                        <Button variant='contained' color='primary' type='submit' fullWidth>Login</Button>
-                        
-                    </form>
-                    
-                </Paper>
-        </Container>
-        </div>
-  )
+          <Typography variant="h5">Admin Login</Typography>
+          <form
+            style={{
+              width: "100%",
+              marginTop: "1rem",
+            }}
+            onSubmit={submitHandler}
+          >
+            <TextField
+              required
+              fullWidth
+              label="Secret Key"
+              type="password"
+              margin="normal"
+              variant="outlined"
+              value={secretKey.value}
+              onChange={secretKey.changeHandler}
+            />
+
+            <Button
+              sx={{
+                marginTop: "1rem",
+              }}
+              variant="contained"
+              color="primary"
+              type="submit"
+              fullWidth
+            >
+              Login
+            </Button>
+          </form>
+        </Paper>
+      </Container>
+    </div>
+  );
 }
 
 export default AdminLogin
