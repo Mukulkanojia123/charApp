960bf29bf5e8efc0899f543af8cf27b2709c5baf
Mukulkanojia123 <mkanojia1996@gmail.com>
2025-06-10 12:44:37 +0530

    messageManagement

--- client/src/components/specific/ChatList.jsx
+++ client/src/components/specific/ChatList.jsx
@@ -7,15 +7,13 @@ const ChatList = ({w='100%', chats=[],chatId, onlineUsers=[],newMessagesAlert=[{
       <Stack width={w} direction={"column"} 
       overflow={'auto'} 
       height={'100%'}
-      // sx={{
-      //   backgroundImage : bgGradient,
-
-      // }}
       >
           {
               chats?.map((data, index)=>{
-                const {avatar,_id, name, groupChat, members} = data;
+                  const {avatar,_id, name, groupChat, members} = data;
+
                   const newMessageAlert = newMessagesAlert.find(({chatId}) => chatId === _id)
+
                   const isOnline = members?.some((member) => onlineUsers.includes(_id));
                   return <ChatItem 
                   index = {index}
--- client/src/pages/admin/MessageManagement.jsx
+++ client/src/pages/admin/MessageManagement.jsx
@@ -1,89 +1,134 @@
-import React, { useEffect, useState } from 'react'
-import AdminLayout from '../../components/layout/AdminLayout'
-import { Avatar, Stack } from '@mui/material'
-import Table from '../../components/shared/Table'
-import { dashboardData } from '../../components/constants/sampleData'
-import {transforImage} from '../../lib/features'
+import { Avatar, Skeleton, Stack } from '@mui/material'
 import moment from 'moment'
+import { useEffect, useState } from 'react'
+import AdminLayout from '../../components/layout/AdminLayout'
+import Table from '../../components/shared/Table'
+import { fileFormat, transforImage } from '../../lib/features'
+import { useFetchData } from '6pp'
 
 
 const columns = [
   {
-    field : 'id',
-    headerName : 'ID',
-    headerClassName : 'table-header',
-    width : 200
+    field: "id",
+    headerName: "ID",
+    headerClassName: "table-header",
+    width: 200,
   },
   {
-    field : 'attachments',
-    headerName : 'Message',
-    headerClassName : 'table-header',
-    width : 200,
+    field: "attachments",
+    headerName: "Attachments",
+    headerClassName: "table-header",
+    width: 200,
+    renderCell: (params) => {
+      const { attachments } = params.row;
+
+      return attachments?.length > 0
+        ? attachments.map((i) => {
+          const url = i.url;
+          const file = fileFormat(url);
+
+          return (
+            <Box>
+              <a
+                href={url}
+                download
+                target="_blank"
+                style={{
+                  color: "black",
+                }}
+              >
+                {RenderAttachment(file, url)}
+              </a>
+            </Box>
+          );
+        })
+        : "No Attachments";
+    },
+  },
+
+  {
+    field: "content",
+    headerName: "Content",
+    headerClassName: "table-header",
+    width: 400,
+  },
+  {
+    field: "sender",
+    headerName: "Sent By",
+    headerClassName: "table-header",
+    width: 200,
     renderCell: (params) => (
-    <Avatar 
-    alt={params.row.name} 
-    src={params.row.avatar}/>
-    )
-  },
-  {
-    field : "content",
-    headerName : "content",
-    headerClassName : 'table-header',
-    width : 400,
-  },
-  {
-    field : 'sender',
-    headerName : 'sent By',
-    headerClassName : 'table-header',
-    width : 200,
-    renderCell: (params) => (
-      <Stack direction={'row'} spacing={'1rem'} alignItem={'center'}>
-    <Avatar 
-    alt={params.row.sender.name} 
-    src={params.row.sender.avatar}/>
+      <Stack direction={"row"} spacing={"1rem"} alignItems={"center"}>
+        <Avatar alt={params.row.sender.name} src={params.row.sender.avatar} />
         <span>{params.row.sender.name}</span>
-        </Stack>
-    )
+      </Stack>
+    ),
   },
   {
-    field : "chat",
-    headerName : "chat",
-    headerClassName : 'table-header',
-    width : 220,
+    field: "chat",
+    headerName: "Chat",
+    headerClassName: "table-header",
+    width: 220,
   },
   {
-    field : "groupsChat",
-    headerName : "groups Chat",
-    headerClassName : 'table-header',
-    width : 100,
+    field: "groupChat",
+    headerName: "Group Chat",
+    headerClassName: "table-header",
+    width: 100,
   },
   {
-    field : "createdAt",
-    headerName : "Time",
-    headerClassName : 'table-header',
-    width : 250,
+    field: "createdAt",
+    headerName: "Time",
+    headerClassName: "table-header",
+    width: 250,
   },
-]
+];
 
 const MessageManagement = () => {
 
-  const [rows, setRows] = useState()
-  useEffect(()=>{
-    setRows(dashboardData.messages.map((i)=>({
-      ...i,
-      id : i._id,
-      sender : {
-        name : i.sender.name,
-        avatar :transforImage(i.sender.avatar,50),
-      },
-      createdAt : moment(i.createdAt).format('MMMM Do YYYY, h:mm:ss a'),
-    })))
-  },[])
+  const { loading, data, error } = useFetchData(
+    `${server}/api/v1/admin/messages`,
+    "dashboard-messages"
+  );
+
+  useErrors([
+    {
+      isError: error,
+      error: error,
+    },
+  ]);
+
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      setRows(
+        data.messages.map((i) => ({
+          ...i,
+          id: i._id,
+          sender: {
+            name: i.sender.name,
+            avatar: transforImage(i.sender.avatar, 50),
+          },
+          createdAt: moment(i.createdAt).format("MMMM Do YYYY, h:mm:ss a"),
+        }))
+      );
+    }
+  }, [data]);
 
   return (
     <AdminLayout>
 
-        <Table heading={'All Messages'} columns={columns} rows={rows} />
+       {loading ? (
+        <Skeleton height={"100vh"} />
+      ) : (
+        <Table
+          heading={"All Messages"}
+          columns={columns}
+          rows={rows}
+          rowHeight={200}
+        />
+      )}
     </AdminLayout>
   )
 }
